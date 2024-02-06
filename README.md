399dc36d1245ed613aeff49ece210d138030995f
Syuugo <pub@s1204.me>
2024-02-06 17:13:00 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+<https://issuetracker.google.com/issues/323771601>
---
+++ lollipop/src/main/java/me/s1204/sample/putInt/lollipop/Activity.java
@@ -0,0 +1,27 @@
+package me.s1204.sample.putInt.lollipop;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.net.Uri;
+import android.provider.Settings;
+import android.widget.Toast;
+
+public class Activity extends android.app.Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        finishAndRemoveTask();
+        if (Settings.System.canWrite(this)) {
+            try {
+                Settings.System.putInt(getContentResolver(), "dcha_state", 1);
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+                Toast.makeText(this, "Failed to write value.", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            Toast.makeText(this, "Please enable toggle switch.", Toast.LENGTH_LONG).show();
+            startActivity(new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS, Uri.parse("package:" + getPackageName())).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+        }
+
+    }
+}
